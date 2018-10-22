4c69d12bfa987952ec97be4e4f989dc7d61abc98
John Young <john.young@thinkcompany.com>
2018-10-22 08:54:30 -0400

    Complete the refactoring of the 'basic-stashing' kata

--- basic-stashing/setup.js
+++ basic-stashing/setup.js
@@ -6,47 +6,19 @@ const kata="basic-stashing"
 spawn('rm -rf exercise/', { shell: true, stdio: 'inherit' });
 console.log(`Setting up exercise \'${kata}\'...`);
 spawn('git init exercise', { shell: true, stdio: 'inherit' });
-spawn('cd exercise && mkdir src', { shell: true, stdio: 'inherit' });
 
-// Make two useful files and commit them
-spawn('echo "** Some USEFUL INFO ** " > exercise/README.txt', { shell: true, stdio: 'inherit' });
-spawn('echo "// awesome C code" > exercise/src/myapp.c', { shell: true, stdio: 'inherit' });
-spawn('echo "// awesome C header" > exercise/src/myapp.h', { shell: true, stdio: 'inherit' });
-spawn('cd exercise && git add src/myapp.* README.txt', { shell: true, stdio: 'inherit' });
-spawn('cd exercise && git commit -m "C code for my app', { shell: true, stdio: 'inherit' });
+spawn('cd exercise && echo "Initial content of the file" > file.txt', { shell: true, stdio: 'inherit' });
+spawn('cd exercise && touch fix.txt', { shell: true, stdio: 'inherit' });
+spawn('cd exercise && git add file.txt fix.txt', { shell: true, stdio: 'inherit' });
+spawn('cd exercise && git commit -m "Initial commit', { shell: true, stdio: 'inherit' });
 
-// Make a useful file, but don't commit it
-spawn('echo "// awesome C library" > exercise/src/mylib.c', { shell: true, stdio: 'inherit' });
+spawn('cd exercise && echo "this file haaasss some typos" > bug.txt', { shell: true, stdio: 'inherit' });
+spawn('cd exercise && git add bug.txt', { shell: true, stdio: 'inherit' });
+spawn('cd exercise && git commit -m "Add bug.txt', { shell: true, stdio: 'inherit' });
 
-// Clutter up the exercise folder with a bunch of useless files
-spawn('touch exercise/src/myapp.c~ exercise/src/oldfile.c~', { shell: true, stdio: 'inherit' });
-spawn('touch exercise/README.txt~', { shell: true, stdio: 'inherit' });
-spawn('cd exercise && mkdir obj', { shell: true, stdio: 'inherit' });
-spawn('touch exercise/obj/mylib.o exercise/obj/mylib.a exercise/obj/myapp.o exercise/obj/a.out', { shell: true, stdio: 'inherit' });
+spawn('cd exercise && echo "some changes I made and staged" >> file.txt', { shell: true, stdio: 'inherit' });
+spawn('cd exercise && git add file.txt', { shell: true, stdio: 'inherit' });
+spawn('cd exercise && echo "some changes I made and did not stage yet" >> file.txt', { shell: true, stdio: 'inherit' });
+spawn('cd exercise && echo "changes I did not stage" > fix.txt', { shell: true, stdio: 'inherit' });
 
-console.log('Exercise setup complete!');
-
-
-
-// TODO: merge the copypasta node setup ABOVE with the desired shell commands BELOW this line.
-
-echo "Initial content of the file" > file.txt
-touch fix.txt
-
-git add file.txt fix.txt
-git commit -m "Initial commit"
-
-
-echo "this file haaasss some typos" > bug.txt
-
-git add bug.txt
-git commit -m "add bug.txt"
-
-echo "some changes I made and staged" >> file.txt
-
-git add file.txt
-
-echo "some changes I made and did not stage yet" >> file.txt
-
-echo "changes I did not stage" > fix.txt
-
+console.log(`Setup of exercise ${kata} complete!`);
