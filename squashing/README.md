3d60395b595dcb2a9db0762351f33c56c0008868
John Young <john.young@thinkcompany.com>
2018-10-24 08:59:06 -0400

    Updates to setup.js and README.md to finalize the exercise

--- squashing/README.md
+++ squashing/README.md
@@ -1,15 +1,36 @@
 # Git Kata: Squash Commits
 
-In this kata I did not worked as I should have, but I would like to clean up my history a bit.
+## Setup:
 
-The six newest commits all tinker with `secondfile.txt` which contains my feature.
+In your terminal, run
+```
+$ node setup.js
+```
+When you see "setup complete", run
+```
+$ cd exercise
+```
 
-While developing, I got a bit hot under the color and used the curse word [BELGIUM](https://hitchhikers.fandom.com/wiki/Belgium).
+In this exercise, you will be returning to a flurry of commits you made the night before.
 
-I would like these commits to be squashed into a single commit.  While  you are at it, remove the curse word BELGIUM.
+You committed `firstfile.txt`. Next, you made six commits to `secondfile.txt` as you gradually found more and more things that should be added to it.
+
+Your commit messages started off relevant, but slowly became lower-quality as you got frustrated.
+
+In your final commit, you got a bit hot under the color and used the curse word [BELGIUM](https://hitchhikers.fandom.com/wiki/Belgium).
+
+You have not yet pushed these commits to an origin repository, so it's appropriate to tidy up the branch a bit.
+
+Squash all the commits touching `secondfile.txt` into a single commit.
+
+While  you are at it, tidy up the commit messages to more accurately summarize things and remove the curse word BELGIUM.
+
+## Setup
+
 
 
 ## Task
+1. Use `git log`, as well as the options you learned in the `basic-staging` exercise, to understand the commits made so far.
 1. _Squash_ the six commits to `secondfile.txt` into one commit, cleaning up the commit msg of one of the commits
 1. How does `git log` look now?
 
--- squashing/setup.js
+++ squashing/setup.js
@@ -11,22 +11,22 @@ spawn('git init exercise', { shell: true, stdio: 'inherit' });
 // Set up the exercise
 spawn(`cd exercise && echo firstfile.txt > firstfile.txt && git add firstfile.txt && git commit -m "Add firstfile.txt with a relevant fact"`, { shell: true, stdio: 'inherit' });
 
-spawn(`cd exercise && echo This is a relevant fact. > secondfile.txt`, { shell: true, stdio: 'inherit' });
+spawn(`cd exercise && echo Fact one > secondfile.txt`, { shell: true, stdio: 'inherit' });
 spawn(`cd exercise && git add secondfile.txt && git commit secondfile.txt -m "Add secondfile.txt with a relevant fact\n"`, { shell: true, stdio: 'inherit' });
 
-spawn(`cd exercise && echo This fact is also relevant. >> secondfile.txt`, { shell: true, stdio: 'inherit' });
+spawn(`cd exercise && echo Fact two >> secondfile.txt`, { shell: true, stdio: 'inherit' });
 spawn(`cd exercise && git add secondfile.txt && git commit -m "Add additional relevancy"`, { shell: true, stdio: 'inherit' });
 
-spawn(`cd exercise && echo Now that I think about it, this is important too >> secondfile.txt`, { shell: true, stdio: 'inherit' });
-spawn(`cd exercise && git add secondfile.txt && git commit -m "Add even more relevancy"`, { shell: true, stdio: 'inherit' });
+spawn(`cd exercise && echo Fact three >> secondfile.txt`, { shell: true, stdio: 'inherit' });
+spawn(`cd exercise && git add secondfile.txt && git commit -m "Add the third fact"`, { shell: true, stdio: 'inherit' });
 
-spawn(`cd exercise && echo Wait, here's the MOST important fact. >> secondfile.txt`, { shell: true, stdio: 'inherit' });
-spawn(`cd exercise && git add secondfile.txt && git commit -m "Yet more relevancy"`, { shell: true, stdio: 'inherit' });
+spawn(`cd exercise && echo Fact four >> secondfile.txt`, { shell: true, stdio: 'inherit' });
+spawn(`cd exercise && git add secondfile.txt && git commit -m "Whoops, forgot about fact four"`, { shell: true, stdio: 'inherit' });
 
-spawn(`cd exercise && echo Hang on, this is the most important fact of all. >> secondfile.txt`, { shell: true, stdio: 'inherit' });
-spawn(`cd exercise && git add secondfile.txt && git commit -m "Add the last relevant fact"`, { shell: true, stdio: 'inherit' });
+spawn(`cd exercise && echo Fact five >> secondfile.txt`, { shell: true, stdio: 'inherit' });
+spawn(`cd exercise && git add secondfile.txt && git commit -m "Here's the last fact, I promise."`, { shell: true, stdio: 'inherit' });
 
-spawn(`cd exercise && echo Here's one more fact >> secondfile.txt`, { shell: true, stdio: 'inherit' });
+spawn(`cd exercise && echo Fact six >> secondfile.txt`, { shell: true, stdio: 'inherit' });
 spawn(`cd exercise && git add secondfile.txt && git commit -m "Darn, I forgot a fact. BELGIUM!"`, { shell: true, stdio: 'inherit' });
 
 console.log(`Setup of exercise ${kata} complete!`);
